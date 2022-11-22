7b1abd0a4b244eafc9c94804ecc6ebd0fe443a24
OdairDias <odair2d@hotmail.com>
2022-11-22 15:17:39 -0300

    parte estrutural
    Signed-off-by: OdairDias <odair2d@hotmail.com>

--- Códigos/pagina_inciall.py
+++ Códigos/pagina_inciall.py
@@ -1,4 +1,12 @@
 from PyQt5 import uic,QtWidgets
+import mysql.connector
+
+bd= mysql.connector.conect(
+    host='localhost',
+    user='rost',
+    passwd='',
+    database="projeto_produtos"
+)
 
 def funcao_principal():
     linha1=pagina_inicial.lineEdit.text()
@@ -10,13 +18,27 @@ def funcao_principal():
     linha4=pagina_inicial.lineEdit_4.text()
     print('Produto:',linha4)
     linha5=pagina_inicial.lineEdit_5.text()
-    print('Quantidade',linha5)
+    print('Preço',linha5)
+    linha6=pagina_inicial.lineEdit_6.text()
+    print('Quantidade',linha6)
+
+    categoria=''
+
     if pagina_inicial.radioButton.isChecked():
         print('Beleza')
+        categoria='Beleza'
     elif pagina_inicial.radioButon_2.isChecked():
         print('Resfriado')
+        categoria='resfriado'
     elif pagina_inicial.radioButon_3.isChecked():
         print('Higiene Pessoal')
+        categoria='higiene Pessoal'
+    # ligando bd com tela
+    cursor=bd.cursor()
+    comando_SQL='insert into produtos_variaveis(Nome,sexo,Idade,produto,preço,Quantidade,categoria) values(%s,%s,%s,%s,%s,%s,%s)'
+    dados=(str(linha1),str(linha2),str(linha3),str(linha4),str(linha5),str(linha6), categoria)
+    cursor.execute(comando_SQL,dados)
+    bd.commit()
 
 app=QtWidgets.QApplication([])
 pagina_inicial=uic.loadUi('pagina_inicial.ui')
@@ -24,3 +46,16 @@ pagina_inicial.pushButton.clicked.connect(funcao_principal)
 
 pagina_inicial.show()
 app.exec()
+# criando as tabelas:
+# create table produtos_variaveis ( 
+    #id INT NOT NULL AUTO_INCREMENT,
+    #Nome VARCHAR(50),  
+    #Sexo VARCHAR(50), 
+    #Idade INT,
+    #produto VARCHAR(30),
+    #preço DOUBLE,
+    #Quantidade INT,
+    #Categoria VARCHAR(30),
+    #primary key (id)
+#);
+#insert into produtos_variaveis (Nome,sexo,Idade,produto,preço,Quantidade,categoria)values ("Odair Dias","M",33,"creme dental",9,1,"saude");
